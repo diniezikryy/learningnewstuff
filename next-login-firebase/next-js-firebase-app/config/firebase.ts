d38b159381ef1111b9c70dd314d34a14a133c229
Dinie Zikry <77114684+diniezikryy@users.noreply.github.com>
2022-07-27 23:13:23 +0800

    initialised auth app

---
+++ next-login-firebase/next-js-firebase-app/config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1J072AiVPT_pB20tqhnjlSVk-hxVul58",
+  authDomain: "next-login-firebase-7ecf6.firebaseapp.com",
+  projectId: "next-login-firebase-7ecf6",
+  storageBucket: "next-login-firebase-7ecf6.appspot.com",
+  messagingSenderId: "329508326152",
+  appId: "1:329508326152:web:5f5854db3e4ed0e3d4babd",
+  measurementId: "G-433S9L7TXY",
+};
+
+const app = initializeApp(firebaseConfig);
